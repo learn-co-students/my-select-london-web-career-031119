ac35b1aec6a82bf56617733f2362052c27e0fbd9
github username <github email address>
2019-02-27 23:41:48 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,8 +2,7 @@ def my_select(collection)
   new_collection = []
   i = 0
   while i < collection.length
-    value = yield(collection[i])
-    if value == true
+    if yield(collection[i]) == true
       new_collection.push(collection[i])
     end
     i += 1
