11246bc4795b55ab67fb23800991aab15be51d5f
Cully Curwen <c.t.g.curwen@gmail.com>
2019-03-04 02:53:46 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,10 @@
 def my_select(collection)
- # your code here!
+ i = 0
+ new_collection = []
+ while i < collection.length
+   if yeild collection[i] == true
+     new_collection << colletion[i]
+   end
+ end
+ new_collection
 end
