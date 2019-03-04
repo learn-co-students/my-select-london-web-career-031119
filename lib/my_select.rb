2c21010aadfaa814a91715f9d70dc761747cddf1
Cully Curwen <c.t.g.curwen@gmail.com>
2019-03-04 02:58:41 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,9 +2,10 @@ def my_select(collection)
  i = 0
  new_collection = []
  while i < collection.length
-   if yeild collection[i] == true
-     new_collection << colletion[i]
+   if yield(collection[i]) == true
+     new_collection << collection[i]
    end
+   i += 1
  end
  new_collection
 end
